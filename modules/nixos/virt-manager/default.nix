5dbd279ee721ba21b6d5097eea95a59cdb49b524
caffeinatedcharlie <git@downgraded.uk>
2024-01-27 03:50:51 +0000

    feat: :package: add virt manager module

---
+++ modules/nixos/virt-manager/default.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+
+let
+  cfg = config.programs.virt-manager;
+in
+{
+  config = lib.mkIf cfg.enable
+    {
+      virtualisation.spiceUSBRedirection.enable = true;
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu = {
+          swtpm.enable = true;
+          ovmf.enable = true;
+          ovmf.packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+    };
+}
