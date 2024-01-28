813260ef2787f422ae970a24f7a3c8712f1a01d6
caffeinatedcharlie <git@downgraded.uk>
2024-01-28 03:57:15 +0000

    feat: :sparkles: add program configurations

---
+++ homes/x86_64-linux/yusof@madyapper/programs.nix
@@ -0,0 +1,51 @@
+{ inputs, pkgs, system, lib, ... }:
+
+let
+  unstable = import inputs.nixpkgs-unstable { system = system; config.allowUnfree = true; };
+  charliepkgs = inputs.charliepkgs;
+in
+{
+  programs = {
+    firefox.enable = true;
+    vscode.enable = true;
+    obs-studio.enable = true;
+    bun.enable = true;
+    vesktop = {
+      enable = true;
+      package = charliepkgs.lib.patchOutput unstable.vesktop ''
+        rm -r $out/share/icons
+        mkdir -p $out/share/icons/hicolor/scalable/apps/
+        cp ${./icons/vesktop.svg} $out/share/icons/hicolor/scalable/apps/vesktop.svg
+      '';
+    };
+    nix-index-database.comma.enable = true;
+    nushell = {
+      enable = true;
+      shellAliases = {
+        switch = "sudo nixos-rebuild switch --fast --print-build-logs";
+        nv = "nix develop --command code";
+        gc = "sudo nix-collect-garbage -d";
+      };
+      extraConfig = ''
+        $env.config.show_banner = false;
+        $env.config.shell_integration = true;
+      '';
+    };
+    git = {
+      enable = true;
+      userName = "yoloyusof";
+      userEmail = "117531058+yoloyusof@users.noreply.github.com";
+      extraConfig = {
+        core.editor = "code --wait";
+        init.defaultBranch = "main";
+        commit.gpgsign = true;
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+        gpg.format = "ssh";
+        credential = {
+          credentialStore = "plaintext";
+          helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
+        };
+      };
+    };
+  };
+}
