0df1908919ddd9111029f6e1ce54cb29e34fc22f
yoloyusof <117531058+yoloyusof@users.noreply.github.com>
2024-01-28 10:37:39 -0500

    fix: :bug: I don't use git ssh

--- homes/x86_64-linux/yusof@madyapper/programs.nix
+++ homes/x86_64-linux/yusof@madyapper/programs.nix
@@ -42,9 +42,6 @@ in
       extraConfig = {
         core.editor = "code --wait";
         init.defaultBranch = "main";
-        commit.gpgsign = true;
-        user.signingkey = "~/.ssh/id_ed25519.pub";
-        gpg.format = "ssh";
         credential = {
           credentialStore = "plaintext";
           helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
