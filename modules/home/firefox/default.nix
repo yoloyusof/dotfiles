2ef78993f4ae8ad66702159d71914bf75f97e0e3
caffeinatedcharlie <git@downgraded.uk>
2024-01-28 11:34:56 +0000

    feat: :sparkles: add firefox home module

---
+++ modules/home/firefox/default.nix
@@ -0,0 +1,28 @@
+{ inputs, config, osConfig, lib, pkgs, ... }: {
+  config = lib.mkIf config.programs.firefox.enable {
+    home.file.".mozilla/firefox/default/chrome/firefox-gnome-theme".source = inputs.firefox-gnome-theme;
+    programs.firefox = {
+      package = inputs.firefox.packages.${pkgs.system}.firefox-nightly-bin.override {
+        cfg = {
+          enableGnomeExtensions = osConfig.services.xserver.desktopManager.gnome.enable;
+          enablePlasmaBrowserIntegration = osConfig.services.xserver.desktopManager.plasma5.enable;
+        };
+      };
+      profiles.default = {
+        isDefault = true;
+        name = "default";
+        id = 0;
+        settings = {
+          # For Firefox GNOME theme:
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "svg.context-properties.content.enabled" = true;
+          "browser.theme.dark-private-windows" = false;
+          "gnomeTheme.hideSingleTab" = true;
+          "gnomeTheme.hideWebrtcIndicator" = true;
+        };
+        userChrome = ''@import "firefox-gnome-theme/userChrome.css";'';
+        userContent = ''@import "firefox-gnome-theme/userContent.css";'';
+      };
+    };
+  };
+}
