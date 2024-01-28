2a32422dba8962354f66d1c24d07df231b1b70a2
caffeinatedcharlie <git@downgraded.uk>
2024-01-28 11:06:27 +0000

    feat: :sparkles: add packages

---
+++ homes/x86_64-linux/yusof@madyapper/packages.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+let
+  unstable = import inputs.nixpkgs-unstable {
+    system = system;
+    config.allowUnfree = true;
+    config.permittedInsecurePackages = [ "electron-25.9.0" ];
+  };
+  charliepkgs = inputs.charliepkgs.packages.${pkgs.system};
+in
+{
+  home.packages = with unstable; [
+    vinegar
+    obsidian
+    inkscape
+    gimp
+    krita
+    piper
+    protonvpn-gui
+    prismlauncher-qt5
+    monophony
+    g4music
+    amberol
+    tor-browser
+    newsflash
+    whois
+  ] ++ [
+    inputs.nix-software-center.packages.${pkgs.system}.nix-software-center
+  ];
+}
+
