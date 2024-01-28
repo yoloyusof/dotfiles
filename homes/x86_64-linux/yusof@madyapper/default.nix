3b195e2f3a80ade36b5b5a177a937af954e61027
caffeinatedcharlie <git@downgraded.uk>
2024-01-28 03:54:12 +0000

    feat: :sparkles: add home for yusof

---
+++ homes/x86_64-linux/yusof@madyapper/default.nix
@@ -0,0 +1,11 @@
+{ inputs, pkgs, system, lib, ... }: {
+  imports =
+    [
+      # Include the packages.
+      ./packages.nix
+      # Include the program settings.
+      ./programs.nix
+    ];
+
+  home.stateVersion = "24.05";
+}
