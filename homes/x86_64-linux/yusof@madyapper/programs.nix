13c87689a5da76ea699892277b98bcb9e942c325
yoloyusof <117531058+yoloyusof@users.noreply.github.com>
2024-02-02 08:47:04 -0500

    feat: :memo: bring back ssh for git

--- homes/x86_64-linux/yusof@madyapper/programs.nix
+++ homes/x86_64-linux/yusof@madyapper/programs.nix
@@ -42,6 +42,9 @@ in
       extraConfig = {
         core.editor = "code --wait";
         init.defaultBranch = "main";
+        commit.gpgsign = true;
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+        gpg.format = "ssh";
         credential = {
           credentialStore = "plaintext";
           helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
